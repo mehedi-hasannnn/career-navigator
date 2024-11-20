320c675913852f8bc549a9d1884407698fb7d09a
mehedi-hasannnn <mhasan2999@gmail.com>
2024-11-20 12:35:42 +0600

    header section added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-
+import logo from "../assets/logoG.png"
+import Typewriter from "typewriter-effect";
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="flex flex-col justify-center items-center">
+            <img className="w-[300px]" src={logo} alt="website logo" />
+            <h2 className="text-gray-400">Navigating Careers, Empowering Dreams</h2>
+
+            <h1 className="text-2xl font-bold text-lime-500 text-center" >
+            <Typewriter
+                options={{
+                strings: ['Navigate Your Career', 'Plan Your Success', 'Achieve Your Dreams'],
+                autoStart: true,
+                loop: true,
+                }}
+            />
+            </h1>
         </div>
     );
 };
