e040f443ebacd5856d74daf83473262a664b34f1
mehedi-hasannnn <mhasan2999@gmail.com>
2024-11-21 13:07:03 +0600

    login logout registration functionality implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import userIcon from "../assets/user.png"
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 const Navbar = () => {
+    const {user, logOut} = useContext(AuthContext);
     return (
         <div className="flex justify-between mt-4 items-center">
-            <div className="font-bold">Career Navigator</div>
+            <div className="font-bold">Career Navigator {user && user.email}</div>
             <div className="nav space-x-1 md:space-x-5">
                 <Link to="/">Home</Link>
                 <Link to="/myprofile">My Profile</Link>
@@ -13,7 +16,10 @@ const Navbar = () => {
                 <div>
                     <img src={userIcon} alt="" />
                 </div>
-                <Link to="/auth/login" className="btn btn-neutral round">Login</Link>
+                {
+                    user && user?.email? ( <button onClick={logOut} className="btn  bg-lime-300 round">LogOut</button> ) : <Link to="/auth/login" className="btn bg-lime-300 round">Login</Link>
+                }
+                
             </div>
         </div>
     );
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBGJh5KVoiEA3dEERQL1UCgskkdvDr6Z8",
+  authDomain: "career-navigator-41fc5.firebaseapp.com",
+  projectId: "career-navigator-41fc5",
+  storageBucket: "career-navigator-41fc5.firebasestorage.app",
+  messagingSenderId: "508700565827",
+  appId: "1:508700565827:web:fd5dad44e86d95c2290ff6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
